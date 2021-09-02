7d3d681ff4e96aaabbafccd9bd6b65f36241761b
jindan <Sidhant Unnithan>
2021-09-02 13:53:08 +0530

    Revert "added till exp4 for networks lab"
    This reverts commit 3ece72b28fbabf747d600a43505a7a1f19c6136a.

--- S7/Networks Lab/Exp 1/tcp_client.py
+++
@@ -1,9 +0,0 @@
-import socket
-s = socket.socket()
-
-print("Sidhant's Client Starting")
-port  = 3000
-s.connect((socket.gethostname(),port))
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
-s.close()
